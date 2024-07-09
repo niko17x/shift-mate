ccc566b73094019016e9b8197f6ee94df8e01ca2
Soko <nshin17@gmail.com>
2024-07-08 22:00:57 -0700

    fix: change route from get to post for login user

--- back-end/src/routes/user.route.js
+++ back-end/src/routes/user.route.js
@@ -12,7 +12,7 @@ import {
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.get("/login", loginUser);
+router.post("/login", loginUser);
 router.get("/logout", logoutUser);
 router.get("/profile/:id", userProfile);
 router.get("/users", users);
