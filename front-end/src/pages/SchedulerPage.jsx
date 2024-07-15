03c47f1829f98055947e5181af4036f3eedb35dc
Soko <nshin17@gmail.com>
2024-07-14 21:51:58 -0700

    feat: Initialize scheduler page

--- front-end/src/pages/SchedulerPage.jsx
+++ front-end/src/pages/SchedulerPage.jsx
@@ -1,5 +1,13 @@
+import MyCalendar from "../components/calendar/CalendarUI.jsx";
+import DisplayDates from "../components/schedulerPage/DisplayDates.jsx";
+
 const SchedulerPage = () => {
-  return <div className="scheduler-page"></div>;
+  return (
+    <div className="scheduler-page">
+      <DisplayDates />
+      {/* <MyCalendar /> */}
+    </div>
+  );
 };
 
 export default SchedulerPage;
