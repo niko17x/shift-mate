ab31d4b05d0c0973c91954030134146da32b024c
Soko <nshin17@gmail.com>
2024-07-21 20:25:18 -0700

    feat: Initialize component for displaying date

--- front-end/src/components/schedulerPage/DisplayDates.jsx
+++ front-end/src/components/schedulerPage/DisplayDates.jsx
@@ -1,22 +1,19 @@
-import { useState } from "react";
 import getDaysOfWeek from "../../utils/getDaysOfWeek";
 import getWeekNumber from "../../utils/getWeekNumber";
+import CreateGrid from "./CreateGrid";
+import useSelectedDate from "../../hooks/calendar/useSelectedDate";
 
-const DisplayDates = () => {
+const DisplayDates = (data, weekNum) => {
   const currentDate = new Date();
   const formattedDate = currentDate.toISOString().split("T")[0];
-
   const defaultWeekNum = getWeekNumber(formattedDate);
   const defaultDays = getDaysOfWeek(formattedDate, defaultWeekNum);
 
+  const { date } = useSelectedDate();
+
   return (
-    <div className="display-dates box">
-      {defaultDays.map((days, index) => (
-        <div className="display-days" key={index}>
-          <p>{days.dayOfWeek}</p>
-          <p>{days.ordinalDate}</p>
-        </div>
-      ))}
+    <div className="display-dates">
+      <CreateGrid data={defaultDays} />
     </div>
   );
 };
