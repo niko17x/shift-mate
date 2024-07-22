377e9c036a41996931dae9592add4e7f56a6945d
Soko <nshin17@gmail.com>
2024-07-21 20:13:32 -0700

    feat: Initalize user profile data updates hook

---
+++ front-end/src/hooks/auth/useFetchUpdateUserProfile.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const useFetchUpdateUserProfile = () => {
+  const [updatedProfileData, setUpdatedProfileData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const updateUserProfile = async (userId, profileDataForm) => {
+    console.log("profileDataForm :", profileDataForm);
+
+    try {
+      const response = await fetch(`/api/user/update-profile/${userId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(profileDataForm),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("data:", JSON.stringify(data, null, 2));
+        setUpdatedProfileData(data);
+      } else {
+        const errorData = await response.json();
+        console.log(
+          "Failed to update profile:",
+          JSON.stringify(errorData, null, 2)
+        );
+        setError(errorData);
+      }
+    } catch (err) {
+      console.log("Error:", err.message);
+      setError({ message: err.message });
+    }
+  };
+
+  return { updateUserProfile, updatedProfileData, error };
+};
+
+export default useFetchUpdateUserProfile;
