65b2c0bc739b8ddef45b6533998ac690078337e5
Soko <nshin17@gmail.com>
2024-08-07 21:20:03 -0700

    feat: Add loading state and refactor toast messages

--- front-end/src/hooks/auth/useFetchUpdateUserProfile.jsx
+++ front-end/src/hooks/auth/useFetchUpdateUserProfile.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 const useFetchUpdateUserProfile = () => {
   const [updatedProfileData, setUpdatedProfileData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const updateUserProfile = async (userId, profileDataForm) => {
-    // console.log("profileDataForm :", profileDataForm.isFullTime);
-    console.log(profileDataForm);
+    setIsLoading(true);
+    setError(null);
 
     try {
       const response = await fetch(`/api/user/update-profile/${userId}`, {
@@ -17,25 +19,30 @@ const useFetchUpdateUserProfile = () => {
         body: JSON.stringify(profileDataForm),
       });
 
-      if (response.ok) {
-        const data = await response.json();
-        // console.log("data:", JSON.stringify(data, null, 2));
-        setUpdatedProfileData(data);
-      } else {
-        const errorData = await response.json();
-        console.log(
-          "Failed to update profile:",
-          JSON.stringify(errorData, null, 2)
-        );
-        setError(errorData);
+      const data = await response.json();
+
+      if (!response.ok) {
+        toast.error(`${data.message}`, {
+          toastId: "profile-update-fail",
+        });
+        setError(data);
+        setIsLoading(false);
+        return;
       }
+
+      setUpdatedProfileData(data);
+      toast.success("Successfully updated profile", {
+        toastId: "profile-update-success",
+      });
     } catch (err) {
       console.log("Error:", err.message);
       setError({ message: err.message });
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  return { updateUserProfile, updatedProfileData, error };
+  return { updateUserProfile, updatedProfileData, error, isLoading };
 };
 
 export default useFetchUpdateUserProfile;
