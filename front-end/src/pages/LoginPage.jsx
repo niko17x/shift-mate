bcfb9e99eea641f8f20cdbe67d25e9b1530422fb
Soko <nshin17@gmail.com>
2024-08-08 21:53:05 -0700

    refactor: Refactored for simple logic during form submit handler

--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -1,34 +1,40 @@
-import { useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import useLoginUser from "../hooks/auth/useLoginUser";
-import { UserContext } from "../context/UserContext";
+import useFetchLoginUser from "../hooks/auth/useFetchLoginUser";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const { loginUser } = useLoginUser();
-  const { setIsUserLoggedIn } = useContext(UserContext);
+  const { loginUser, error } = useFetchLoginUser();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const isLoginSuccess = await loginUser(username, password);
+    if (!username || !password) {
+      toast.error("Please fill in all fields", {
+        toastId: "login-empty-fields",
+      });
+      return;
+    }
 
-      if (isLoginSuccess) {
-        setIsUserLoggedIn(true);
-        setUsername("");
-        setPassword("");
-        navigate("/");
-      }
-    } catch (err) {
-      console.log("Error while logging in", err.message);
+    const isLoggedIn = await loginUser(username, password);
+
+    if (isLoggedIn) {
+      toast.success("Logged in successfully", {
+        toastId: "login-success",
+      });
+      navigate("/");
+    } else if (error) {
+      toast.error(error, {
+        toastId: "login-fail",
+      });
     }
   };
 
-  console.log("LoginPage component rendered");
+  // console.log("LoginPage component rendered");
 
   return (
     <div className="login-page container box">
