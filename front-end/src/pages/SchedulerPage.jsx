6f7dfc9b600598cda1ef4c836101ac40b7520fed
Soko <nshin17@gmail.com>
2024-08-19 11:51:06 -0700

    refactor: Move local storage retrieval to another component

--- front-end/src/pages/SchedulerPage.jsx
+++ front-end/src/pages/SchedulerPage.jsx
@@ -2,6 +2,12 @@ import MyCalendar from "../components/calendar/CalendarUI.jsx";
 import DisplayDates from "../components/schedulerPage/DisplayDates.jsx";
 
 const SchedulerPage = () => {
+  const isUserLoggedIn = localStorage.getItem("user");
+
+  if (!isUserLoggedIn) {
+    return <div>Sign in to view scheduler</div>;
+  }
+
   return (
     <div className="scheduler-page">
       <DisplayDates />
