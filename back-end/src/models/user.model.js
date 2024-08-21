da0eff7efc11cc76b4b83583feaeec1c33ef173b
Soko <nshin17@gmail.com>
2024-08-21 14:53:18 -0700

    refactor: Remove username property from model

--- back-end/src/models/user.model.js
+++ back-end/src/models/user.model.js
@@ -12,10 +12,6 @@ const userSchema = mongoose.Schema(
       type: String,
       // required: true,
     },
-    username: {
-      type: String,
-      // required: true,
-    },
     email: {
       type: String,
       // required: true,
