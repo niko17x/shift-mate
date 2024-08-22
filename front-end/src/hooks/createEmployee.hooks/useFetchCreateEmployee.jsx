bedba5f3a94ba35404e87e9e7f93610f8e216716
Soko <nshin17@gmail.com>
2024-08-21 21:37:17 -0700

    refactor: Remove loading state as it is not needed

--- front-end/src/hooks/createEmployee.hooks/useFetchCreateEmployee.jsx
+++ front-end/src/hooks/createEmployee.hooks/useFetchCreateEmployee.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
 
 const useFetchCreateEmployee = () => {
-  const [isLoading, setIsLoading] = useState(false);
   const [errors, setErrors] = useState([]);
 
   const createEmployee = async (formData) => {
-    setIsLoading(true);
     setErrors([]);
 
     try {
@@ -20,26 +18,21 @@ const useFetchCreateEmployee = () => {
       const data = await response.json();
 
       if (!response.ok) {
-        setIsLoading(false);
         setErrors(data.errors || ["An error has occurred."]);
         return { success: false, error: data.message };
       }
 
-      setIsLoading(false);
       setErrors([]);
       return { success: true, data };
     } catch (error) {
-      setIsLoading(false);
       const errorMessage =
         "An unexpected event has occurred. Please try again.";
       setErrors([errorMessage]);
       return { success: false, errorMessage };
-    } finally {
-      setIsLoading(false);
     }
   };
 
-  return { isLoading, createEmployee, errors };
+  return { createEmployee, errors };
 };
 
 export default useFetchCreateEmployee;
