8194ceacb4b5ecc1f86fae14fcb6a883a59de2e7
Soko <nshin17@gmail.com>
2024-08-21 21:39:11 -0700

    feat: Add new route to handle fetching employee list

--- back-end/src/routes/employee.route.js
+++ back-end/src/routes/employee.route.js
@@ -1,8 +1,12 @@
 import express from "express";
 // import protect from "../middlewares/protect.middleware.js";
-import { createEmployee } from "../controllers/employee.controller.js";
+import {
+  createEmployee,
+  getAllEmployees,
+} from "../controllers/employee.controller.js";
 const router = express.Router();
 
 router.post("/create-employee", createEmployee);
+router.get("/all-employees", getAllEmployees);
 
 export default router;
