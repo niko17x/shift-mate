28301c6131d297e7b33ace093de5c25141b19736
Soko <nshin17@gmail.com>
2024-08-22 22:51:10 -0700

    feat: Add employee list fetching and employee delete function logic

--- front-end/src/components/employeePage/DisplayEmployees.jsx
+++ front-end/src/components/employeePage/DisplayEmployees.jsx
@@ -1,23 +1,62 @@
 import { useEffect, useState } from "react";
-import useFetchAllEmployees from "../../hooks/createEmployee.hooks/useFetchAllEmployees";
+import useFetchAllEmployees from "../../hooks/manageEmployees.hooks/useFetchAllEmployees";
+import useFetchDeleteEmployee from "../../hooks/manageEmployees.hooks/useFetchDeleteEmployee";
+import useFetchEmployeeProfile from "../../hooks/manageEmployees.hooks/useFetchEmployeeProfile";
 import Loader from "../common/Loader";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 
 const DisplayEmployees = ({ toggleModal, closeModal, openModal }) => {
   const [employeeList, setEmployeeList] = useState([]);
-  const { isLoading, fetchAllEmployees } = useFetchAllEmployees();
+  const { isLoading: isLoadingEmployees, fetchAllEmployees } =
+    useFetchAllEmployees();
+  const {
+    isLoading: isDeleting,
+    error: errorDeleting,
+    deleteEmployee,
+    data,
+  } = useFetchDeleteEmployee();
+
+  // TODO => Decide on UI to view employee profile data as admin. What makes most sense?
+  // ! Experimenting w/ employee profile data retrieval (to be deleted):
+  const { employeeProfile } = useFetchEmployeeProfile();
+  const handleThisClick = async (emp) => {
+    console.log(emp._id);
+    const bar = await employeeProfile(emp._id);
+    console.log(bar);
+  };
+  // !
 
   useEffect(() => {
     const getEmployeeList = async () => {
-      const employees = await fetchAllEmployees();
-
-      if (employees) setEmployeeList(employees.data);
+      if (toggleModal) {
+        const employees = await fetchAllEmployees();
+        if (employees) {
+          setEmployeeList(employees.data);
+        }
+      }
     };
 
     getEmployeeList();
-  }, [toggleModal]);
+  }, [toggleModal, data]);
 
-  if (isLoading) {
-    return <Loader loading={isLoading} />;
+  const handleDelete = async (employeeId) => {
+    await deleteEmployee(employeeId._id);
+
+    if (errorDeleting || !data) {
+      toast.error("Failed to delete employee", {
+        toastId: "delete-employee-fail",
+      });
+      return;
+    }
+
+    toast.success("Employee successfully deleted", {
+      toastId: "delete-employee-success",
+    });
+  };
+
+  if (isLoadingEmployees || isDeleting) {
+    return <Loader loading={isLoadingEmployees} />;
   }
 
   return (
@@ -32,15 +71,23 @@ const DisplayEmployees = ({ toggleModal, closeModal, openModal }) => {
           {employeeList &&
             employeeList.map((emp) => (
               <div className="employee-list" key={emp._id}>
-                <p>
-                  {emp.lastName}, <span>{emp.firstName}</span>{" "}
-                </p>
-                <p>{emp.email}</p>
-                <p>{emp.jobTitle}</p>
-                <p>{emp.isFullTime}</p>
-                <p>{emp.tenure}</p>
-                <p>{emp.eCode}</p>
-                <div className="divider"></div>
+                <div className="notification is-info">
+                  <button
+                    className="delete"
+                    onClick={() => handleDelete(emp)}
+                  ></button>
+                  <Link to={`/profile/${emp._id}`}>
+                    <p>
+                      {emp.lastName}, <span>{emp.firstName}</span>{" "}
+                    </p>
+                  </Link>
+                  <p>{emp.email}</p>
+                  <p>{emp.jobTitle}</p>
+                  <p>{emp.isFullTime}</p>
+                  <p>{emp.tenure}</p>
+                  <p>{emp.eCode}</p>
+                  <button onClick={() => handleThisClick(emp)}>click me</button>
+                </div>
               </div>
             ))}
         </div>
@@ -56,5 +103,5 @@ const DisplayEmployees = ({ toggleModal, closeModal, openModal }) => {
 
 export default DisplayEmployees;
 
-// TODO => 1. Delete employee in list // 2. Edit employee detail by providing link in list.
-// Keep styling simple in the beginning. Business logic is what matters at first.
+// * Keep styling simple in the beginning. Business logic is what matters at first.
+// TODO => Manage case for quick employee deletion causing issues. Ask GPT.
