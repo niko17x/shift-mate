084a5290ba313e54cd955eb618fccae97cae75e1
Soko <nshin17@gmail.com>
2024-08-22 22:52:35 -0700

    feat: Add routes for deleting and getting employee profile data

--- back-end/src/routes/employee.route.js
+++ back-end/src/routes/employee.route.js
@@ -2,11 +2,15 @@ import express from "express";
 // import protect from "../middlewares/protect.middleware.js";
 import {
   createEmployee,
+  deleteEmployee,
+  employeeProfile,
   getAllEmployees,
 } from "../controllers/employee.controller.js";
 const router = express.Router();
 
 router.post("/create-employee", createEmployee);
 router.get("/all-employees", getAllEmployees);
+router.get("/profile/:id", employeeProfile);
+router.delete("/delete/:id", deleteEmployee);
 
 export default router;
